15157fe68c9736cfc6ffd385ea9154c7a5bd7b78
levente99 <max.levente99@gmail.com>
2021-02-18 20:09:44 +0100

    Fixed route naming and separated question and answer endpoints

---
+++ TestMEApi/Controllers/QuestionsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestMEApi.Data;
+using TestMEApi.Models;
+
+namespace TestMEApi.Controllers
+{
+    [ApiController]
+    [Route("api/questions")]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly TestMEApiContext _context;
+
+        public QuestionsController(TestMEApiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/questions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Question>> GetQuestion(int id)
+        {
+            var question = await _context.Question.FindAsync(id);
+
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return question;
+        }
+
+        // POST: api/questions
+        [HttpPost]
+        public async Task<ActionResult<Question>> PostQuestion(Question question)
+        {
+            _context.Question.Add(question);
+            var result = await _context.SaveChangesAsync();
+            if(result == 0)
+            {
+                return StatusCode(500);
+            }
+            return CreatedAtAction("GetQuestion", new { id = question.Id }, question);
+        }
+    }
+}
---
+++ TestMEApi/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestMEApi.Models
+{
+    public class User : IdentityUser
+    {
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //public override string Id { get; set; }
+
+        [ForeignKey("Role")]
+        public int RoleId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public int Xp { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+    }
+}
